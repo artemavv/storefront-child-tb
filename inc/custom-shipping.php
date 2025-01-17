b6aa061ec7462875f3799a7a8128e73c27cea218
artem <artem.the.freelancer@gmail.com>
2025-01-17 21:23:52 +0100

    Updated delivery description and added country names

--- inc/custom-shipping.php
+++ inc/custom-shipping.php
@@ -1022,17 +1022,13 @@ class TannyBunny_Custom_Shipping_Helper extends TannyBunny_Custom_Shipping_Core 
 	public function get_delivery_date_estimate( $mode = 'standard', $warehouse_restriction = '' ) {
 		$estimate_in_days = $this->get_delivery_estimate( $mode, $warehouse_restriction );
 		
-		
-		//echo('$estimate_in_days<pre>' . print_r( $estimate_in_days , 1 ) . '</pre>' );
-		
-		
 		$from_timestamp = time() + $estimate_in_days['from'] * DAY_IN_SECONDS;
 		$to_timestamp   = time() + $estimate_in_days['to'] * DAY_IN_SECONDS;
 		
 		$from_month = date( 'M', $from_timestamp );
 		$to_month   = date( 'M', $to_timestamp );
 		
-		if ( $from_month === $estimate_delivery_for_warehouseto_month ) { // output like "Nov 13-25"
+		if ( $from_month === $to_month ) { // output like "Nov 13-25"
 			$from = date( 'M j', $from_timestamp );
 			$to   = date( 'j', $to_timestamp );
 			$out  = "$from-$to";
@@ -1046,6 +1042,37 @@ class TannyBunny_Custom_Shipping_Helper extends TannyBunny_Custom_Shipping_Core 
 		return $out;
 	}
 	
+	
+	/**
+	 * Returns a string with estimated dates of delivery. e.g "20 Nov-30 Dec"
+	 * 
+	 * @param string $warehouse_restriction "am" or "us"
+	 * @return string e.g "20 Nov-30 Dec"
+	 */
+	public function get_delivery_date_range( $warehouse_restriction = '' ) {
+		
+		$standard_estimate = $this->get_delivery_estimate( 'standard', $warehouse_restriction );
+		$express_estimate = $this->get_delivery_estimate( 'express', $warehouse_restriction );
+		
+		$from_timestamp = time() + $express_estimate['from'] * DAY_IN_SECONDS; // minimum possible date
+		$to_timestamp   = time() + $standard_estimate['to'] * DAY_IN_SECONDS; // maximum possible date
+		
+		$from_month = date( 'M', $from_timestamp );
+		$to_month   = date( 'M', $to_timestamp );
+		
+		if ( $from_month === $to_month ) { // output like "Nov 13-25"
+			$from = date( 'M j', $from_timestamp );
+			$to   = date( 'j', $to_timestamp );
+			$out  = "$from-$to";
+			
+		} else { // output like "Sep 13-Oct 25"
+			$from = date( 'M j', $from_timestamp );
+			$to   = date( 'M j', $to_timestamp );
+			$out  = "$from-$to";
+		}
+		
+		return $out;
+	}
 	
 	
 	/**
@@ -1188,9 +1215,11 @@ class TannyBunny_Custom_Shipping_Helper extends TannyBunny_Custom_Shipping_Core 
 		
 		$check_mark_icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false" class="check-mark"><path d="M9.059 20.473 21.26 6.15l-1.52-1.298-10.8 12.675-4.734-4.734-1.414 1.414z"></path></svg>';
 		
-		$delivery_date_estimated = $this->get_delivery_date_estimate( 'standard' );
+		$country = $this->get_country_name( $this->customer_country );
 		
-		$line_about_delivery_estimate = '<li>' . $check_mark_icon . ' Arrives soon! Get it by <span class="tooltip-notice" data-tooltip="' . self::RETURN_NOTICE . '">' . $delivery_date_estimated . '</span> if you order today</li>';
+		$delivery_date_estimated = $this->get_delivery_date_range();
+		
+		$line_about_delivery_estimate = '<li>' . $check_mark_icon . ' Arrives to ' . $country . ' by <span class="tooltip-notice" data-tooltip="' . self::RETURN_NOTICE . '">' . $delivery_date_estimated . '</span> if you order today</li>';
 		$line_about_delivery_conditions = '<li>' . $check_mark_icon . ' Returns and exchanges accepted</li>';
 		
 		$out = '<ul class="shipping-details">'
@@ -1199,6 +1228,269 @@ class TannyBunny_Custom_Shipping_Helper extends TannyBunny_Custom_Shipping_Core 
 		. '</ul>';
 		
 		return $out;
+	}
+	
+	public function get_country_name( $country_code = 'JP' ) {
+		
+		$countries = [
+			"Afghanistan" => "AF",
+			"Åland Islands" => "AX",
+			"Albania" => "AL",
+			"Algeria" => "DZ",
+			"American Samoa" => "AS",
+			"Andorra" => "AD",
+			"Angola" => "AO",
+			"Anguilla" => "AI",
+			"Antarctica" => "AQ",
+			"Antigua and Barbuda" => "AG",
+			"Argentina" => "AR",
+			"Armenia" => "AM",
+			"Aruba" => "AW",
+			"Australia" => "AU",
+			"Austria" => "AT",
+			"Azerbaijan" => "AZ",
+			"Bahamas" => "BS",
+			"Bahrain" => "BH",
+			"Bangladesh" => "BD",
+			"Barbados" => "BB",
+			"Belarus" => "BY",
+			"Belgium" => "BE",
+			"Belize" => "BZ",
+			"Benin" => "BJ",
+			"Bermuda" => "BM",
+			"Bhutan" => "BT",
+			"Bolivia" => "BO",
+			//"Bonaire, Sint Eustatius and Saba" => "BQ",
+			"Bosnia and Herzegovina" => "BA",
+			"Botswana" => "BW",
+			"Bouvet Island" => "BV",
+			"Brazil" => "BR",
+			"British IOT" => "IO", //"British Indian Ocean Territory" => "IO",
+			"Brunei Darussalam" => "BN",
+			"Bulgaria" => "BG",
+			"Burkina Faso" => "BF",
+			"Burundi" => "BI",
+			"Cambodia" => "KH",
+			"Cameroon" => "CM",
+			"Canada" => "CA",
+			"Cape Verde" => "CV",
+			"Cayman Islands" => "KY",
+			"Central African Republic" => "CF",
+			"Chad" => "TD",
+			"Chile" => "CL",
+			"China" => "CN",
+			"Christmas Island" => "CX",
+			"Cocos (Keeling) Islands" => "CC",
+			"Colombia" => "CO",
+			"Comoros" => "KM",
+			"Congo" => "CG",
+			"Congo" => "CD",
+			"Cook Islands" => "CK",
+			"Costa Rica" => "CR",
+			"Côte d'Ivoire" => "CI",
+			"Croatia" => "HR",
+			"Cuba" => "CU",
+			"Curaçao" => "CW",
+			"Cyprus" => "CY",
+			"Czech Republic" => "CZ",
+			"Denmark" => "DK",
+			"Djibouti" => "DJ",
+			"Dominica" => "DM",
+			"Dominican Republic" => "DO",
+			"Ecuador" => "EC",
+			"Egypt" => "EG",
+			"El Salvador" => "SV",
+			"Equatorial Guinea" => "GQ",
+			"Eritrea" => "ER",
+			"Estonia" => "EE",
+			"Ethiopia" => "ET",
+			"Falkland Islands (Malvinas)" => "FK",
+			"Faroe Islands" => "FO",
+			"Fiji" => "FJ",
+			"Finland" => "FI",
+			"France" => "FR",
+			"French Guiana" => "GF",
+			"French Polynesia" => "PF",
+			"French Southern Territories" => "TF",
+			"Gabon" => "GA",
+			"Gambia" => "GM",
+			"Georgia" => "GE",
+			"Germany" => "DE",
+			"Ghana" => "GH",
+			"Gibraltar" => "GI",
+			"Greece" => "GR",
+			"Greenland" => "GL",
+			"Grenada" => "GD",
+			"Guadeloupe" => "GP",
+			"Guam" => "GU",
+			"Guatemala" => "GT",
+			"Guernsey" => "GG",
+			"Guinea" => "GN",
+			"Guinea-Bissau" => "GW",
+			"Guyana" => "GY",
+			"Haiti" => "HT",
+			"Heard Island and McDonald Islands" => "HM",
+			"Vatican City" => "VA",
+			"Honduras" => "HN",
+			"Hong Kong" => "HK",
+			"Hungary" => "HU",
+			"Iceland" => "IS",
+			"India" => "IN",
+			"Indonesia" => "ID",
+			"Iran" => "IR",
+			"Iraq" => "IQ",
+			"Ireland" => "IE",
+			"Isle of Man" => "IM",
+			"Israel" => "IL",
+			"Italy" => "IT",
+			"Jamaica" => "JM",
+			"Japan" => "JP",
+			"Jersey" => "JE",
+			"Jordan" => "JO",
+			"Kazakhstan" => "KZ",
+			"Kenya" => "KE",
+			"Kiribati" => "KI",
+			"North Korea" => "KP",
+			"Korea" => "KR",
+			"Kuwait" => "KW",
+			"Kyrgyzstan" => "KG",
+			"Laos" => "LA",
+			"Latvia" => "LV",
+			"Lebanon" => "LB",
+			"Lesotho" => "LS",
+			"Liberia" => "LR",
+			"Libya" => "LY",
+			"Liechtenstein" => "LI",
+			"Lithuania" => "LT",
+			"Luxembourg" => "LU",
+			"Macao" => "MO",
+			"Macedonia" => "MK",
+			"Madagascar" => "MG",
+			"Malawi" => "MW",
+			"Malaysia" => "MY",
+			"Maldives" => "MV",
+			"Mali" => "ML",
+			"Malta" => "MT",
+			"Marshall Islands" => "MH",
+			"Martinique" => "MQ",
+			"Mauritania" => "MR",
+			"Mauritius" => "MU",
+			"Mayotte" => "YT",
+			"Mexico" => "MX",
+			"Micronesia" => "FM", //"Micronesia, Federated States of" => "FM",
+			"Moldova" => "MD",
+			"Monaco" => "MC",
+			"Mongolia" => "MN",
+			"Montenegro" => "ME",
+			"Montserrat" => "MS",
+			"Morocco" => "MA",
+			"Mozambique" => "MZ",
+			"Myanmar" => "MM",
+			"Namibia" => "NA",
+			"Nauru" => "NR",
+			"Nepal" => "NP",
+			"Netherlands" => "NL",
+			"New Caledonia" => "NC",
+			"New Zealand" => "NZ",
+			"Nicaragua" => "NI",
+			"Niger" => "NE",
+			"Nigeria" => "NG",
+			"Niue" => "NU",
+			"Norfolk Island" => "NF",
+			"Northern Mariana Islands" => "MP",
+			"Norway" => "NO",
+			"Oman" => "OM",
+			"Pakistan" => "PK",
+			"Palau" => "PW",
+			"Palestine, State of" => "PS",
+			"Panama" => "PA",
+			"Papua New Guinea" => "PG",
+			"Paraguay" => "PY",
+			"Peru" => "PE",
+			"Philippines" => "PH",
+			"Pitcairn" => "PN",
+			"Poland" => "PL",
+			"Portugal" => "PT",
+			"Puerto Rico" => "PR",
+			"Qatar" => "QA",
+			"Réunion" => "RE",
+			"Romania" => "RO",
+			"Russia" => "RU",
+			"Rwanda" => "RW",
+			"Saint Barthélemy" => "BL",
+			"Saint Helena" => "SH",
+			"Saint Kitts and Nevis" => "KN",
+			"Saint Lucia" => "LC",
+			"Saint Martin" => "MF",
+			"Saint Pierre and Miquelon" => "PM",
+			"Saint Vincent and the Grenadines" => "VC",
+			"Samoa" => "WS",
+			"San Marino" => "SM",
+			"Sao Tome and Principe" => "ST",
+			"Saudi Arabia" => "SA",
+			"Senegal" => "SN",
+			"Serbia" => "RS",
+			"Seychelles" => "SC",
+			"Sierra Leone" => "SL",
+			"Singapore" => "SG",
+			"Sint Maarten" => "SX",
+			"Slovakia" => "SK",
+			"Slovenia" => "SI",
+			"Solomon Islands" => "SB",
+			"Somalia" => "SO",
+			"South Africa" => "ZA",
+			"South Georgia" => "GS",
+			"South Sudan" => "SS",
+			"Spain" => "ES",
+			"Sri Lanka" => "LK",
+			"Sudan" => "SD",
+			"Suriname" => "SR",
+			"Svalbard and Jan Mayen" => "SJ",
+			"Eswatini" => "SZ",
+			"Sweden" => "SE",
+			"Switzerland" => "CH",
+			"Syria" => "SY",
+			"Taiwan" => "TW",
+			"Tajikistan" => "TJ",
+			"Tanzania" => "TZ",
+			"Thailand" => "TH",
+			"Timor-Leste" => "TL",
+			"Togo" => "TG",
+			"Tokelau" => "TK",
+			"Tonga" => "TO",
+			"Trinidad and Tobago" => "TT",
+			"Tunisia" => "TN",
+			"Turkey" => "TR",
+			"Turkmenistan" => "TM",
+			"Turks and Caicos Islands" => "TC",
+			"Tuvalu" => "TV",
+			"Uganda" => "UG",
+			"Ukraine" => "UA",
+			"United Arab Emirates" => "AE",
+			"United Kingdom" => "GB",
+			"United States" => "US",
+			"United States Minor Outlying Islands" => "UM",
+			"Uruguay" => "UY",
+			"Uzbekistan" => "UZ",
+			"Vanuatu" => "VU",
+			"Venezuela" => "VE",
+			"Viet Nam" => "VN",
+			"Virgin Islands, British" => "VG",
+			"Virgin Islands, U.S." => "VI",
+			"Wallis and Futuna" => "WF",
+			"Western Sahara" => "EH",
+			"Yemen" => "YE",
+			"Zambia" => "ZM",
+			"Zimbabwe" => "ZW"
+		];
+		
+		if ( in_array( $country_code, $countries ) ) {
+		
+			$codes = array_flip($countries);
+			return $codes[$country_code];
+		}
+		
+		return $country_code;
 	}
 }
 
